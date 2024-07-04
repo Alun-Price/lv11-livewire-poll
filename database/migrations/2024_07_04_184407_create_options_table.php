fde27603779ef8d11d3205393f6a351824264255
Alun Price <alunprice089@gmail.com>
2024-07-04 21:41:19 +0100

    Defined polls, options and votes table columns and migrated

--- database/migrations/2024_07_04_184407_create_options_table.php
+++ database/migrations/2024_07_04_184407_create_options_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Poll;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +14,10 @@ return new class extends Migration
     {
         Schema::create('options', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
+
+            $table->foreignIdFor(Poll::class)->constrained();
         });
     }
 
